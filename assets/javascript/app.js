68654637650ddb879645dd189d538809eb21289a
J Tsai <jtsai972@gmail.com>
2019-09-24 23:27:21 -0500

    just added a note for something I wanted to do

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,29 @@
-// on click fieldset, get fieldset id for submission
+// ? Comments styled with Better Comments VSCode Extension
+// * All these comments will go away as code gets filled in.
+//variables go somewhere up here
+
+// * Setup
+//ToDo: set up repeatable timer, timestop & count functions
+
+// * On Start
+//Todo: on click start, display timer, unhide form & first fieldset
+
+// * Form
+// ToDo: on click fieldset, get fieldset id for submission
+// Todo: Submit Button displays result
+
+// * Setting up results
+//Todo: h2 with "Time's up", Correct/Incorrect depending on cause
+// ? Correct answer will probably be based on :radio:checked.val() = correct; isCorrect ? correct : incorrect
+// Note to self: learn how to use ternary operators properly and find places to use them
+//ToDo: Display correct answer 
+
+//ToDo: reset timer, hide result, unhide next fieldset
+
+// * Bonus stuff
+//Todo: experiment with adding and removing classes dynamically to try to animate stuff lightly
+//ToDo: add details about answers to result based on question id i.e stuff about why moon is coldest.
+
+
+//Functions go somewhere down here
+
